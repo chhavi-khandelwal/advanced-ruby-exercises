98855c31ecc270b04126fa00394feb676c60677b
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-20 15:27:56 +0530

    method call added for another string object and removed interpolation

--- singleton_method/bin/main.rb
+++ singleton_method/bin/main.rb
@@ -1,17 +1,21 @@
 string_instance = 'singleton method'
+another_string_instance = 'regular string'
 
 # Demonstrating something.method_name
 def string_instance.demo
-  puts "#{ self }"
+  puts self
 end
 
 string_instance.demo
+# another_string_instance.demo -> shows undefined method error
 
 # Demonstrating class << self
+
 class << string_instance
   def demo
-    puts "#{ self }"
+    puts self
   end
 end
 
 string_instance.demo
+# another_string_instance.demo -> shows undefined method error
